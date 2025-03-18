0ed4924eaad848bb4bc0aef98bd8eab057ef7dd6
jve005 <jorgenveim@gmail.com>
2025-03-18 15:43:59 +0100

    Working SpriteController

--- Assets/Scripts/SpriteController.cs
+++ Assets/Scripts/SpriteController.cs
@@ -1,21 +1,29 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class SpriteController : MonoBehaviour
 {
-    private SpriteRenderer _characterSprite;
+    public SpriteRenderer characterSprite;
     //Reference to total amount of sprites for a character
     public Sprite[] sprites;
     //Current sprite on display
-    private int currentSprite = 0;
+    public int currentSprite = 0;
     
     void Start()
     {
-        _characterSprite = gameObject.GetComponent<SpriteRenderer>();
+        characterSprite = gameObject.GetComponent<SpriteRenderer>();
     }
 
     
     void Update()
     {
-        
+        //Controls what the current sprite is (temporary)
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            currentSprite += 1;
+            if (currentSprite > 4)
+                currentSprite = 0;
+            characterSprite.sprite = sprites[currentSprite];
+        }
     }
 }
