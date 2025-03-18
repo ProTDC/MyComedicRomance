33eb5fcd4d349818c3d304efbb32cc8a14719613
jve005 <jorgenveim@gmail.com>
2025-03-18 14:47:41 +0100

    Progress on SpriteController

--- Assets/Scripts/SpriteController.cs
+++ Assets/Scripts/SpriteController.cs
@@ -2,13 +2,18 @@ using UnityEngine;
 
 public class SpriteController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private SpriteRenderer _characterSprite;
+    //Reference to total amount of sprites for a character
+    public Sprite[] sprites;
+    //Current sprite on display
+    private int currentSprite = 0;
+    
     void Start()
     {
-        
+        _characterSprite = gameObject.GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         
