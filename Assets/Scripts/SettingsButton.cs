6847c73f98c4dde76f6e38316b1a7890cd204356
Drianna-79 <sofmolnet@gmail.com>
2025-03-26 14:24:18 +0100

    Functional Gameplay Settings Button
    BUG: Saved changes don't get transferred to another scene (Main Menu is fine, but my test scene is jank with saving)

--- Assets/Scripts/SettingsButton.cs
+++ Assets/Scripts/SettingsButton.cs
@@ -1,17 +1,24 @@
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingsButton : MonoBehaviour
 {
     //This script is for settings button in gameplay mode
-    [SerializeField] private GameObject gameSettingsButton;
+    [SerializeField] private Button gameSettingsButton;
     [SerializeField] private GameObject settingsMenu;
+
+    private void Start()
+    {
+        gameSettingsButton.GetComponent<Button>();
+    }
 
     public void EnterIngameSettings()
     {
         Debug.Log("Entering Settings");
-        gameSettingsButton.SetActive(false);
+        gameSettingsButton.interactable = false;
         settingsMenu.SetActive(true);
     }
 
-    public void ExitIngameSettings() {gameSettingsButton.SetActive(true);}
+    public void ExitIngameSettings() {gameSettingsButton.interactable = true;}
 }
